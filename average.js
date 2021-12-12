c3bbd156c6a5668fb4e52c998f976fb29e47d5c6
Sonal Jain <52230497+sonaljain067@users.noreply.github.com>
2021-12-12 14:28:38 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,6 +1,6 @@
 
 function average(numbers) {
-  var reduced=numbers.map(function(obj)) {return obj;})
+  var reduced=numbers.map(function(obj) {return obj;})
                       .reduce((p, c)=> p + c, 0)/ numbers.length;
   //return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
 return reduced;
